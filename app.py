fd4225ad08a30cdbb0264bcbe3605a02cbb7804b
DeanMcG <g00369547@gmit.ie>
2021-12-04 19:51:27 +0000

    Reading stock info from database

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
 #Imports
 import pandas as pd
 import numpy as np
-import streamlit as st
 import matplotlib.pyplot as plt
+import streamlit as st
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+
+from sklearn.preprocessing import MinMaxScaler
+
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize']=20,10
+
+from sklearn.preprocessing import MinMaxScaler
+
+import pymongo
+from pymongo import MongoClient
+import dns
+
+
 
 
 #Read csv file into variable df
-df = pd.read_csv("GOOG.csv")
-current_df = pd.read_csv("GOOG.csv")
-forecast_df = pd.read_csv("GOOG.csv")
+
+myclient = pymongo.MongoClient("mongodb+srv://admin:Password@stockcluster.cuzo7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mydb = myclient["Stocks"]
+mycol = mydb["GOOG"]
+
+df=pd.DataFrame(list(mycol.find()))
+df = df.drop('_id', axis = 1)
+
+current_df=pd.DataFrame(list(mycol.find()))
+current_df = current_df.drop('_id', axis = 1)
+
+forecast_df=pd.DataFrame(list(mycol.find()))
+forecast_df = forecast_df.drop(['_id'], axis = 1)
+
+
 
 #For debugging purposes
 #print(df)
@@ -29,6 +57,8 @@ st.table(data = current_df.tail())
 def plot_current():
     st.header("Current Graph")
     current_df["Date"]=pd.to_datetime(current_df.Date,format="%Y-%m-%d")
+
+    current_df['Close'] = current_df['Close'].astype(float)
 
     fig = plt.figure(figsize=(12,8))
 
@@ -51,6 +81,8 @@ st.table(data = forecast_df.tail())
 def plot_forecast():
     st.header("Forecast Graph")
     forecast_df["Date"]=pd.to_datetime(forecast_df.Date,format="%Y-%m-%d")
+
+    forecast_df['Close'] = forecast_df['Close'].astype(float)
 
     fig = plt.figure(figsize=(12,8))
 
