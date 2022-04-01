779ce88c15f217a4d092a7dd41929b2398fbbef8
DeanMcG <g00369547@gmit.ie>
2022-04-01 21:01:30 +0100

    User Login Authentication Complete

--- user/routes.py
+++ user/routes.py
@@ -1,7 +1,17 @@
+
 from flask import Flask
 from app import app
-from user.models import User #User class from models.py
+from user.models import User
 
-@app.route('/user/signup', methods =['GET'])
+#Routes
+@app.route('/user/signup', methods=['POST'])
 def signup():
-    return User().signup()
+  return User().signup()
+
+@app.route('/user/signout')
+def signout():
+  return User().signout()
+
+@app.route('/user/login', methods=['POST'])
+def login():
+  return User().login()
